6ca87be1a9eed280bc219c224146961d91dabf0a
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2020-11-03 19:30:08 +0900

    単体コード追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
          validates :nickname, presence: true
-         validates :lastname, presence: true
-         validates :firstname, presence: true
-         validates :lastname_kana, presence: true
-         validates :firstname_kana, presence: true
+         validates :lastname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :firstname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :lastname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+         validates :firstname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
 
          has_many :items
