58782390ac1d3ff02b603cb30486a6317e51668e
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2020-11-06 18:46:07 +0900

    一覧表示実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @items = Item.all
   end
 
   def new
@@ -19,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :exposition, :category_id, :state_id, :delivery_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :exposition, :category_id, :state_id, :delivery_id, :area_id, :days_id, :price, :image).merge(user_id: current_user.id)
   end
 end
