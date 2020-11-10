6215c146644817ee6a9404d8180df7d46682e245
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2020-11-10 15:15:28 +0900

    テストコード実装

--- app/models/user_domain.rb
+++ app/models/user_domain.rb
@@ -9,15 +9,15 @@ class UserDomain
     validates :post_number
     validates :town
     validates :number
-    #validates :build
     validates :photo_number
   end
+
   validates :prefecture_id, numericality: { other_than: 1 }
+  validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  validates :photo_number, format: { with: /\A\d{11}\z/ }
+  #validates :photo_number, format: { with: less_than_or_equal_to: 11 }
 
   def save
     Domain.create(post_number: post_number, town: town, number: number, photo_number: photo_number, prefecture_id: prefecture_id)
   end
 end
-
-
-#build: bulid
--- spec/factories/user_domains.rb
+++ spec/factories/user_domains.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user_domain do
-    post_number
-    town
-    number
-    photo_number
-    prefecture_id
+    post_number {000-3434}
+    town {"横浜市緑区"}
+    number {"青山1-1-1"}
+    photo_number {0120500500}
+    prefecture_id { 2 }
   end
 end
